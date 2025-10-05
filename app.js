e44bbdecc69b78fb675daf7a77dea4386f646d1e
yashbitcode <yashkumar.sre.177@gmail.com>
2025-10-05 21:19:27 +0530

    tried event loop based output questions

--- app.js
+++ app.js
@@ -1,29 +1,183 @@
 const fs = require("fs");
-const a = 100;
+// // const a = 100;
 
-setImmediate(() => console.log("set-immediate"));
+// setImmediate(() => console.log("set-immediate"));
+// setTimeout(() => console.log("timeout expired"), 0);
 
-const p = new Promise((res) => res("Promise"));
-p.then(console.log);
+// Promise.resolve("Promise").then(console.log);
 
-fs.readFile("./file.txt", "utf-8", (_, data) => console.log(data));
+// fs.readFile("./file.txt", "utf-8", (_, data) => {
+//     // setTimeout(() => console.log("2nd timer"), 0);
 
-setTimeout(() => console.log("timeout expired"), 0);
+//     // process.nextTick(() => console.log("2nd process.nextTick"));
+
+//     // setImmediate(() => console.log("2nd set-immediate"));
+
+//     console.log(data);
+// });
+
+// process.nextTick(() => {
+//     process.nextTick(() => console.log("inner nexttick"));
+
+//     console.log("process.nextTick");
+// });
+
+// nexttick =>
+// promise =>
+
+// t -
+// p -
+// c -
+// c -
+
+// last line
+// process.nextTick
+// innertick
+// promise
+// timeout expired
+// set-immediate
+// fil data
+
+// function printA() {
+//     console.log("a = ", a);
+// }
+
+// printA();
+
+// nexttick =>
+// promise =>
+
+// t -
+// p -
+// c -
+// c -
+
+// last line...
+// process.nextTick
+// promise
+// timeout expired
+// set-immediate
+// file data
+// 2nd process.nextTick
+// 2nd timer
+// 2nd set-immediate
+
+// const a = 100;
+
+// setImmediate(() => console.log("setImmediate"));
+
+// fs.readFile("./file.txt", "utf8", (_, data) => {
+//     console.log(data);
+// });
+
+// setTimeout(() => console.log("Timer expired"), 0);
+
+// function printA() {
+//     console.log("a=", a);
+// }
+
+// printA();
+
+// console.log("Last line of the file.");
+
+// OUTPUT
+// a = 100
+// last line...
+// timer expired
+// setImmediate
+// <file_content>
+
+// const a = 100;
+
+// setImmediate(() => {
+//     console.log("setImmediate");
+//     process.nextTick(() => console.log("process.nextTick setImm."));
+// });
+
+// Promise.resolve("Promise").then(console.log);
+
+// fs.readFile("./file.txt", "utf8", (_, data) => {
+//     console.log(data);
+// });
+
+// setTimeout(() => {
+//     console.log("Timer expired");
+//     process.nextTick(() => console.log("process.nextTick settime."));
+// }, 0);
+
+// process.nextTick(() => console.log("process.nextTick"));
+
+// function printA() {
+//     console.log("a=", a);
+// }
+
+// printA();
+
+// console.log("Last line of the file.");
+
+// OUTPUT
+// a= 100
+// Last line...
+// process.nextTick
+// Promise
+// Timer expired
+// process.nextTick settime
+// setImmediate
+// process.nextTick setImm
+// <file content>
+
+setImmediate(() => console.log("setImmediate"));
+
+setTimeout(() => console.log("Timer expired"), 0);
+
+Promise.resolve("promise").then(console.log);
+
+fs.readFile("./file.txt", "utf8", (_, data) => {
+    setTimeout(() => console.log("2nd timer"), 0);
+
+    process.nextTick(() => console.log("2nd nextTick"));
+
+    setImmediate(() => console.log("2nd setImmediate"));
+
+    console.log(data);
+});
 
 process.nextTick(() => console.log("process.nextTick"));
 
-function printA() {
-    console.log("a = ", a);
-}
+console.log("Last line of the file.");
 
-printA();
-console.log("last line...");
+// OUTPUT
+// Last line of the file.
+// process.nextTick
+// promise
+// Timer expired
+// setImmediate
+// <file content>
+// 2nd nextTick
+// 2nd setImmediate
+// 2nd timer
 
+setImmediate(() => console.log("setImmediate"));
 
-// promise { [Function (anonymous)] } // not printed
-//a = 100
-// last line
-// next tick
-// timeout 
-// setimmm
-// file content
+setTimeout(() => console.log("Timer expired"), 0);
+
+Promise.resolve("promise").then(console.log);
+
+fs.readFile("./file.txt", "utf8", (_, data) => {
+    console.log(data);
+});
+
+process.nextTick(() => {
+    process.nextTick(() => console.log("inner nextTick"));
+    console.log("process.nextTick");
+});
+
+console.log("Last line of the file.");
+
+// OUTPUT
+// process.nextTick
+// inner nextTick
+// promise
+// Timer expired
+// setImmediate
+// <file content>
