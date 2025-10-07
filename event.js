6ff5d279f5b2abc17b75b01d459d04624b5b8a47
yashbitcode <yashkumar.sre.177@gmail.com>
2025-10-07 19:40:42 +0530

    events....

--- event.js
+++ event.js
@@ -2,12 +2,58 @@ const EventEmitter = require("node:events");
 
 const emitter = new EventEmitter();
 
-emitter.on("greet-me", (fullName) => {
-    console.log("hi bro", fullName);
+// emitter.on("greet-me", (fullName) => {
+//     console.log("hi bro", fullName);
+// });
+
+// emitter.addListener("greet-me", (fullName) => {
+//     console.log("hi world", fullName);
+// });
+
+// emitter.emit("greet-me", "YashBitCode");
+
+const eventCnt = {
+    login: 0,
+    purchase: 0,
+    logout: 0,
+    "profile-update": 0
+};
+
+emitter.on("user-login", (fullname) => {
+    console.log("user logged in: ", fullname);
+    eventCnt.login++;
 });
 
-emitter.addListener("greet-me", (fullName) => {
-    console.log("hi world", fullName);
+emitter.on("user-logout", () => {
+    console.log("user logged out");
+    eventCnt.logout++;
 });
 
-emitter.emit("greet-me", "YashBitCode");
+emitter.on("user-purchase", () => {
+    console.log("purchased");
+    eventCnt.purchase++;
+});
+
+emitter.on("user-profile-update", () => {
+    console.log("profile updated");
+    eventCnt["profile-update"]++;
+});
+
+emitter.on("get-summary", () => {
+    const { login, purchase, logout } = eventCnt;
+
+    console.log("login: ", login);
+    console.log("logout: ", logout);
+    console.log("purchase: ", purchase);
+    console.log("profile update: ", eventCnt["profile-update"]);
+});
+
+emitter.emit("user-login", "YashBit");
+emitter.emit("user-logout");
+emitter.emit("user-purchase");
+emitter.emit("user-profile-update");
+emitter.emit("user-profile-update");
+emitter.emit("get-summary");
+
+emitter.removeAllListeners();
+// emitter.emit("get-summary");
